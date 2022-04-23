b7adf10d960c7cdf881dafce106c845970261611
Tinatin <omurovatinatin@gmail.com>
2022-04-23 11:40:31 +0600

    refactor: remove deploy

--- src/index.js
+++ src/index.js
@@ -7,19 +7,17 @@ import { createStore , compose, applyMiddleware} from "redux";
 import {rootReducer} from './redux/rootReducer'
 import { Provider } from 'react-redux';
 import { spamFilter } from './redux/middleware';
-import { HashRouter } from 'react-router-dom';
+
 
 
 const store = createStore(rootReducer, compose(applyMiddleware(thunk, spamFilter), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 
 
 ReactDOM.render(
-  <HashRouter basename={process.env.PUBLIC_URL}>
+
     <Provider store={store}>
       <App />,
-    </Provider>
-  </HashRouter>,
- 
+    </Provider>,
 
   document.getElementById('root')
 );
