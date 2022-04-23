d59ef489ac543b6321aee687890c75760534f63d
Tinatin <omurovatinatin@gmail.com>
2022-04-23 11:06:56 +0600

    feat: add script deploy

--- src/index.js
+++ src/index.js
@@ -7,15 +7,19 @@ import { createStore , compose, applyMiddleware} from "redux";
 import {rootReducer} from './redux/rootReducer'
 import { Provider } from 'react-redux';
 import { spamFilter } from './redux/middleware';
+import { HashRouter } from 'react-router-dom';
 
 
 const store = createStore(rootReducer, compose(applyMiddleware(thunk, spamFilter), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 
 
 ReactDOM.render(
-  <Provider store={store}>
-     <App />,
-  </Provider>,
+  <HashRouter basename={process.env.PUBLIC_URL}>
+    <Provider store={store}>
+      <App />,
+    </Provider>
+  </HashRouter>,
+ 
 
   document.getElementById('root')
 );
